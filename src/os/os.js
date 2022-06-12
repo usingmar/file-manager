9bea60e9f925ef995d0b78693ca8e401b5d40d04
usingmar <uselastmar@gmail.com>
2022-06-13 00:31:41 +0300

    fix4

--- src/os/os.js
+++ src/os/os.js
@@ -2,12 +2,12 @@ import sys from 'os'
 
 export const os = (currentPath, input) => {
     switch(input[0]){
-    case --EOL: console.log(sys.EOL);
-    case --cpus: console.log(sys.cpus());
-    case --homedir: console.log(sys.homedir());
-    case --username: console.log(sys.userInfo.username);
-    case --architecture: console.log(sys.arch());
-    default: console.log('\nInvalid Input\n');
+    case '--EOL':{ console.log(sys.EOL); break;}
+    case '--cpus': {console.log(sys.cpus()); break;}
+    case '--homedir': {console.log(sys.homedir()); break;}
+    case '--username': {console.log(sys.userInfo().username); break;}
+    case '--architecture': {console.log(sys.arch()); break;}
+    default: {console.log('\nInvalid Input\n'); break;}
     }
     return currentPath;
 }
