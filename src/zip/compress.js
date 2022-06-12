392d14a83d5c38528d4e9e4023de3772bdd8d9cc
usingmar <uselastmar@gmail.com>
2022-06-13 01:16:12 +0300

    fix6

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,14 +1,26 @@
 import {createBrotliCompress} from 'zlib'
-import { createReadStream, createWriteStream, write } from 'fs';
+import { createReadStream, createWriteStream} from 'fs';
 import { pipeline } from 'stream';
 import path from 'path';
+import fs from 'fs'
 
 export const compress = (currentPath, input) => {
+    let readFileStream = null;
+    let writeFileStream = null;
+
     try{
-    if(path.isAbsolute(input[0])) {const readFileStream = createReadStream(input[0]);}
-    else {const readFileStream = createReadStream(fs.realpathSync(currentPath + path.sep + input[0]));}
-    if(path.isAbsolute(input[1])) { const writeFileStream = createWriteStream('./files\\archive.gz');}
-    else { const writeFileStream = createWriteStream(fs.realpathSync(currentPath + path.sep + input[1]));}
+    if(path.isAbsolute(input[0])){readFileStream = createReadStream(input[0]);}
+    else {readFileStream = createReadStream(fs.realpathSync(currentPath + path.sep + input[0]));}
+    if(path.isAbsolute(input[1])) { 
+        let fd = fs.openSync(input[1],'w');       
+        if(fd)fs.closeSync(fd);
+        writeFileStream = createWriteStream(input[1]);
+    }
+    else {
+        let fd = fs.openSync(currentPath + path.sep + input[1],'w');       
+        if(fd)fs.closeSync(fd);
+         writeFileStream = createWriteStream(fs.realpathSync(currentPath + path.sep + input[1]));
+        }
 
     const compress = createBrotliCompress();
 
@@ -20,8 +32,9 @@ export const compress = (currentPath, input) => {
             if(error) throw error;
         }
     );
-    }catch{
-        console.log('Zip operation failed');
+    }catch(error){
+        console.log(error);//'Zip operation failed
         return currentPath;
     }
+    return currentPath;
 };
