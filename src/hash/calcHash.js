fccca85294e9a7096c094a548fabfc5a6c7f61f1
usingmar <uselastmar@gmail.com>
2022-06-13 00:00:32 +0300

    fix3

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,8 +2,13 @@ import crypto from 'crypto'
 import fs from 'fs'
 
 export const calcHash = (currentPath, input) => {
+        try{
 if(path.isAbsolute(input[0])) {
         console.log(crypto.createHash('sha256').update(fs.readFileSync(input[0], 'utf8')).digest('hex'));
 }else console.log(crypto.createHash('sha256').update(fs.readFileSync(fs.realpathSync(currentPath + path.sep + input[0]), 'utf8')).digest('hex'));
+        }catch{
+                console.log('FS operation failed');
+                return currentPath;
+        }
 return currentPath;
 }
